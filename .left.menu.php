4a9640552d5d7c95a15a95827feaf10312d5d3ac
Elena <lena1687@gmail.com>
2017-11-15 13:08:56 +0600

    additional files

---
+++ .left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Каталог", 
+		"/#catalog-anchor", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Доставка и оплата", 
+		"/#shipping-anchor", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/#contacts-anchor", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
